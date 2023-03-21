52e68126bf7b8f83ca390adf2054e049ce175c98
Apostolis Stamatis <apostolis.stamatis1@gmail.com>
2023-03-21 11:19:15 +0200

    Enhance test for blockchain

--- testing_files/test_blockchain.py
+++ testing_files/test_blockchain.py
@@ -1,10 +1,20 @@
 from noobcash import Blockchain
 
 
-def test_blockchain():
+def test_empty_validity():
+    nodes = 5
+    blockchain = Blockchain.Blockchain(nodes)
+    assert blockchain.validate_chain()
+
+
+def test_genesis_validity():
     nodes = 5
     bootstrap_address = "bootstrap_address"
     blockchain = Blockchain.Blockchain(nodes)
-    assert blockchain.validate_chain()
     blockchain.GenesisBlock(bootstrap_address)
     assert blockchain.validate_chain()
+    assert len(blockchain.chain) == 1
+    genesis_block = blockchain.chain[0]
+    assert genesis_block.previousHash == "1"
+    assert genesis_block.nonce == 0
+
